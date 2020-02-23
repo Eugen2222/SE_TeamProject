3651b35aa7858949724e5126fd0bc6a1b7d4d360
Wei-Ting Lin <2434295l@student.gla.ac.uk>
2020-02-23 18:16:29 +0000

    UPDATE PD

--- PTTSystem/ControllerPackage/PDController.java
+++ PTTSystem/ControllerPackage/PDController.java
@@ -1,6 +1,9 @@
 package ControllerPackage;
 import GUIPackage.View;
 import ModelPackage.Model;
+
+import java.awt.BorderLayout;
+
 import DefaultPackage.Run;
 public class PDController extends Controller {
 
@@ -8,5 +11,17 @@ public class PDController extends Controller {
 		super(model, view, logc);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void defaultPage() {
+		view.bar.buildPDBar(model.getUser()[0], model.getUser()[1]);
+		view.frame.buildMainPanel();
+		view.framePanel.add(view.barPanel, BorderLayout.WEST);
+		view.barPanel.setVisible(true);
+		view.logoutBN.addActionListener(this.logC);
+		System.out.println("add"+view.logoutBN.getActionListeners()[0]);
+		view.main.buildCreateClassPanel();
+		view.framePanel.add(view.createClassPanel, BorderLayout.CENTER);
+		view.add(view.framePanel);
+		view.pList.add(view.framePanel);
+	}
 }
